2c737ca8d31059e9047ff68bb0e277effc53e4f3
Kurtina <kurtdekker@gmail.com>
2021-06-26 09:43:07 -0700

    First proof-of-concept

---
+++ kurtgui/Assets/KurtGUITextReplacements/KurtGUIText.cs
@@ -0,0 +1,115 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2021 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KurtGUIText : MonoBehaviour
+{
+	[Multiline]
+	public string text;
+
+	public TextAnchor anchor;
+	public TextAlignment alignment;
+
+	public Color color = Color.white;
+
+	public float w = 0.10f;
+	public float h = 0.05f;
+
+	public int fontSize;
+
+	const string SkinBaseResourcesPath = "KurtGUI/SKINBASE";
+	static GUISkin SKINBASE
+	{
+		get
+		{
+			return Resources.Load<GUISkin>( SkinBaseResourcesPath);
+		}
+	}
+
+	GUISkin modifiedSkin;
+
+	void OnGUI()
+	{
+		Vector3 position = transform.position;
+
+		Rect r = KurtGUIRects.ScreenRect( 0, 0, w, h);
+
+		float x = position.x;
+		float y = 1.0f - position.y;
+
+		Vector2 center = KurtGUIRects.ScreenPos( x, y);
+		switch( alignment)
+		{
+		case TextAlignment.Left :
+			center.x += r.width / 2;
+			break;
+		case TextAlignment.Right :
+			center.x -= r.width / 2;
+			break;
+		}
+
+		r.center = center;
+
+		if (!modifiedSkin)
+		{
+			modifiedSkin = Instantiate<GUISkin>( SKINBASE);
+
+			var style = modifiedSkin.label;
+
+			style.alignment = anchor;		// caution on naming!
+
+			style.active.textColor = color;
+
+			if (fontSize > 0)
+			{
+				style.fontSize = fontSize;
+			}
+
+			modifiedSkin.label = style;
+		}
+
+		if (modifiedSkin)
+		{
+			GUI.skin = modifiedSkin;
+		}
+
+		GUI.color = color;
+
+		GUI.Label( r, text);
+	}
+}
